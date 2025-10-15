f7d1a17ba4a9ca638045b17c6287b5e1468ebd97
Yun-Hwan Jeon <yunsol267@gmail.com>
2025-10-15 23:24:43 +0900

    feat: 구분자를 이용한 합 계산

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -2,54 +2,49 @@ package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.List;
-import java.util.ArrayList;
-
 public class Application {
     public static void main(String[] args) {
-        String input;
-        int result = 0;
-        char seperator = ',';
+        // 사용자 입력 값 설정
+        String input = Console.readLine();
 
-        // 출력 양식 1
-        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        // 기본 구분자 설정
+        String seperator = "[,:]";
 
-        // 사용자 입력
-        input = Console.readLine();
+        // 기본 출력 값 설정
+        int sum = 0;
 
-        // 커스텀 구분자
-        if(input.startsWith("//")) {
-            seperator = input.charAt(2);
-            System.out.println("커스텀 구분자: " + seperator);
+        // 입력 값이 "//"로 시작하고, "\\n"이 포함되어 있을 때
+        if(input.startsWith("//") && input.contains("\\n")) {
+            // 커스텀 구분자 종료 위치
+            int index = input.indexOf("\\n");
+
+            // 커스텀 구분자 추출
+            String customSeperator = input.substring(2, index);
+
+            // 커스텀 구분자 이후로 사용자 입력 값 변경
+            input = input.substring(index + 2);
+
+            // 기본 구분자에 커스텀 구분자 추가
+            seperator = "[,:]|" + customSeperator;
         }
 
-        // 숫자 합 계산
-        int sum = getNumberArray(input).stream().mapToInt(Integer::intValue).sum();
+        // 구분자를 이용해 사용자 입력 값 자르기
+        String[] numbers = input.split(seperator);
+
+        for(String number : numbers){
+            // 빈 값이 아닐 때
+            if(!number.trim().isEmpty()){
+                // String 값에서 Int 형식으로 변환
+                int num = Integer.parseInt(number.trim());
+
+                // 음수면 IllegalArgumentException 던지기
+                if(num < 0) throw new IllegalArgumentException();
+
+                // 정상적인 값으면 합에 추가
+                sum += num;
+            }
+        }
 
         System.out.println("결과 : " + sum);
-    }
-
-    // 문자열 배열에서 숫자를 분리하고, 배열에 추가하여 반환
-    public static List<Integer> getNumberArray(String input) {
-        // 문자열에서 음수, 양수 둘 다 추출하여 문자열 배열에 추가
-        String[] numbers = input.split("[^0-9-]+");
-
-        List<Integer> values = new ArrayList<>();
-
-        for(String number : numbers) {
-            if(number.isEmpty()) {
-                continue;
-            }
-
-            int num = Integer.parseInt(number);
-
-            if(num < 0) {
-                throw new IllegalArgumentException();
-            }
-
-            values.add(num);
-        }
-
-        return values;
     }
 }
